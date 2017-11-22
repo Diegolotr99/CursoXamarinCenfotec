2901445959efb5bb7f4fc01dd19e93379e7e9c31
Jean Carlos <berrocal13@gmail.com>
2017-11-22 08:21:11 -0600

    Laboratorio para PC

---
+++ Laboratorio1CenfotecPC/Laboratorio1CenfotecPC/Laboratorio1CenfotecPC/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Laboratorio1CenfotecPC.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Laboratorio1CenfotecPC
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            NavigationPage navigation = new NavigationPage(new HomePage());
+
+            App.Current.MainPage = new MasterDetailPage
+            {
+                Master = new HomeMenu(),
+                Detail = navigation
+            };
+
+            //MainPage = new HomePage();
+        }
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
